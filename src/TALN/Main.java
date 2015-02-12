38f3a339deaf602a018d5550d276db4f9a8ce6ab
Astyan42 <deveaux162@hotmail.com>
2015-02-12 13:47:31 +0100

    first import

---
+++ src/TALN/Main.java
@@ -0,0 +1,14 @@
+package TALN;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if(args.length != 1){
+            System.out.println("Usage");
+            System.out.println("You must at least have 1 argument which is a filepath or a real word");
+        }
+
+        // firstly we're gonna implement the word only implementation
+        Word.Interpretation(args[0]);
+    }
+}
---
+++ src/TALN/Word.java
@@ -0,0 +1,35 @@
+package TALN;
+
+/**
+ * Created by jdeveaux on 12/02/2015.
+ */
+public class Word {
+    public String getWordString() {
+        return Word;
+    }
+
+    private String Word;
+
+
+    public Word(String word) {
+        this.Word = word;
+    }
+
+    public void getWordTermination(){
+
+    }
+    public void getWordroot(){
+
+    }
+    public void analyzeWord(){
+
+    }
+
+    public static void Interpretation(String word) {
+         Word wordToAnalyze = new Word(word);
+        // Do Stuff
+
+        // Print result
+
+    }
+}

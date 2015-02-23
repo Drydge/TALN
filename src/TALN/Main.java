2e135e7cc2dd5d3f424d58784f75c22de13038f3
Astyan42 <deveaux162@hotmail.com>
2015-02-23 13:48:57 +0100

    Arbre
    Comme on l'a vu sur Skype ensemble, je pousse les modifs que je t'ai
    expliquée sur l'arbre

--- src/TALN/Main.java
+++ src/TALN/Main.java
@@ -1,20 +1,25 @@
 package TALN;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        TerminationTree tree= new TerminationTree();
-        tree.makeTerminationTree();
-        try {
-            tree.ShowTerminationTree();
-        }
-        catch (Exception ignored){
-
-        }
         if(args.length != 1){
             System.out.println("Usage");
             System.out.println("You must at least have 1 argument which is a filepath or a real word");
+            return;
         }
+        try {
+            TerminationTree tree= TerminationTree.loadTerminationTree();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // tree.makeTerminationTree();
+        // tree.ShowTerminationTree();
+
+
         // firstly we're gonna implement the word only implementation
         // Word.Interpretation(args[0]);
     }
--- src/TALN/TerminationTree.java
+++ src/TALN/TerminationTree.java
@@ -1,94 +1,85 @@
 package TALN;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class TerminationTree {
-    public char root='!';
-    public TerminationTree child;
-    public TerminationTree brother;
-
-    public TerminationTree subTree;
+    private char root;
+    // contient l'ensemble des types de mots : verbe premier groupe, nom, etc
+    private ArrayList<String> definition;
+    private ArrayList<TerminationTree> children;
 
     public TerminationTree(){
         this.root='#';
+        this.children = new ArrayList<TerminationTree>();
     }
-    public void setRoot(char c){
-        this.root=c;
-    }
-    public void makeTerminationTree(){
-        try {
-            InputStream ips = new FileInputStream("/home/drydge/Documents/S6/Langage Naturel /TALN/src/TALN/terminationFile.txt");
-            InputStreamReader ipsr = new InputStreamReader(ips);
-            BufferedReader br = new BufferedReader(ipsr);
-            String ligne;
-            this.setRoot('^');
-            this.child=new TerminationTree();
-            this.brother=new TerminationTree();
-            // try to put termination in TerminationTree
-            while ((ligne = br.readLine()) != null) {
-                // split all inline entry in an entry table
-                String[] entry =ligne.split(",");
-                char[] termination;
-                for(int i = 1; i < entry.length; i++) {
-                    this.subTree=this.child;
 
-                    // each termination in table of char
-                    termination = entry[i].toCharArray();
-                    int j = termination.length-1;
-                    if (termination[j]!=' '){
-                        while (j >=0) {
-                            if (this.subTree.root=='#'){
-                                this.subTree.setRoot(termination[j]);
-                                this.subTree.child=new TerminationTree();
-                                this.subTree.brother=new TerminationTree();
-                                System.out.println("tree-r--" + termination[j]);
-                                this.subTree=this.subTree.child;
-                                j--;
-                            }else if (this.subTree.root==termination[j]) {
-                                System.out.println("tree-c--" + termination[j]);
-                                this.subTree=this.subTree.child;
-                                j--;
-                            }else{
-                                this.subTree=this.subTree.brother;
-                            }
-                        }
-                        this.addChild('*');
-                    }
-                }
+    public TerminationTree(char l){
+        this.root = l;
+        this.children = new ArrayList<TerminationTree>();
+    }
+
+    public boolean hasChild(){
+        return this.children != null && this.children.size() > 0;
+    }
+
+    public static TerminationTree loadTerminationTree() throws IOException {
+        InputStream ips = new FileInputStream("./src/TALN/terminationFile.txt");
+        InputStreamReader ipsr = new InputStreamReader(ips);
+        BufferedReader br = new BufferedReader(ipsr);
+        String line;
+        TerminationTree toReturn = new TerminationTree();
+        // Pour chaque ligne de notre fichier, nous voulons:
+        // 1 - récupérer toutes les terminaisons et les inscrire dans l'arbre
+        // 2 - flaguer la feuille comme terminaison d'un groupe ( nom féminin, nom singulier, verbe du premier groupe, etc )
+        while ((line = br.readLine()) != null){
+            String[] split = line.split(",");
+            String group = split[0];
+            String[] terminations = new String[split.length - 1];
+            System.arraycopy(split, 1, terminations, 0, split.length - 1);
+            for(String termination: terminations){
+                // - Vérifier si l'un des fils correspond à la dernière lettre de la terminaison
+                InsertTermination(toReturn, termination, group);
             }
         }
-        catch (Exception e){
-            System.out.println(e.toString());
+
+        return toReturn;
+    }
+
+    private static void InsertTermination(TerminationTree currentLeaf, String termination, String group) {
+        // Si ma récursion est terminée je m'arrête;
+        if(termination.equals("")){
+            currentLeaf.definition.add(group);
+            return;
+        }
+        // S'il y a un fils qui est le même que la dernière lettre je continue sur ce fils
+        char lastChar = termination.charAt(termination.length() - 1);
+        if(currentLeaf.checkChild(lastChar)){
+            InsertTermination(currentLeaf.getChild(lastChar), termination.substring(0, termination.length() - 1), group);
+        }
+        //sinon j'ajoute le noeud et je continue
+        else{
+            TerminationTree nextNode = new TerminationTree(lastChar);
+            currentLeaf.children.add(nextNode);
+            InsertTermination(nextNode, termination.substring(0, termination.length() - 1), group);
         }
     }
 
-    public void ShowTerminationTree(){
-        System.out.println("<- TERMINATION TREE ->");
-        this.ShowTerminationTreeRec(this,1);
-        System.out.println("\n>--------------------<");
-    }
-
-    private void addChild(char c){
-        if (this.root=='#') {
-            this.brother=new TerminationTree();
-            this.child=new TerminationTree();
-            this.root=c;
-        }
-        else this.brother.addChild(c);
-    }
-
-    private void ShowTerminationTreeRec(TerminationTree tree,int n ) {
-        for (int i = 1; i < n; i++) {
-            System.out.print(' ');
-        }
-        if (tree.root!='#'){
-            System.out.println(tree.root);
-            tree.ShowTerminationTreeRec(tree.child, n + 1);
-            if (tree.brother.root!='#'){
-                System.out.println();
-                tree.ShowTerminationTreeRec(tree.brother,n);
+    private boolean checkChild(char c) {
+        for(TerminationTree child : this.children){
+            if(child.root == c){
+                return true;
             }
         }
+        return false;
     }
 
+    private TerminationTree getChild(char root){
+        for(TerminationTree child : this.children){
+            if(child.root == root){
+                return child;
+            }
+        }
+        return null;
+    }
 }
